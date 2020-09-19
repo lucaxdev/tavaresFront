85508949f43d304ad12dee66d390bcec5a98516b
lucas cavalcante <48697826+lucaxdev@users.noreply.github.com>
2020-09-19 09:10:48 -0400

    new commit
    new

---
+++ precache-manifest.d01a6371d8c58fc7d1583f9a202ab4cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844d68dabc2f3b67244e9873fa769c12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2b1f54d9d88a967aa28",
+    "url": "/static/css/main.6a9ba68d.chunk.css"
+  },
+  {
+    "revision": "61e41dbd2e6a216e1fb8",
+    "url": "/static/js/2.bbd72606.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bbd72606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b1f54d9d88a967aa28",
+    "url": "/static/js/main.ccca79cc.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  }
+]);
